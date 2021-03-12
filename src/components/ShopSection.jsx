b7d30df4f30804f400dd85a79c4b0d39467fa1a6
Phil <pjguitar15@gmail.com>
2021-03-12 15:17:58 +0800

    added products

--- src/components/ShopSection.jsx
+++ src/components/ShopSection.jsx
@@ -1,9 +1,196 @@
 import React from 'react'
-
+import product1 from '../Assets/product1.jpg'
+import product2 from '../Assets/product2.jpg'
+import product3 from '../Assets/product3.jpg'
+import product4 from '../Assets/product4.jpg'
+import product5 from '../Assets/product5.png'
+import product6 from '../Assets/product6.jpg'
+import product8 from '../Assets/product8.jpg'
+import product9 from '../Assets/product9.jpg'
+import product10 from '../Assets/product10.jpg'
+import product11 from '../Assets/product11.jpg'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 const ShopSection = () => {
   return (
-    <div>
-      
+    <div className='m-0 shop-section'>
+      <Container>
+        <h2 className='text-center'>Welcome to our shop</h2>
+        <p className='text-center'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <Row className='my-5'>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product1} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product2} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product3} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product4} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product5} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product6} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product8} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product9} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product10} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg='3' className='mt-3'>
+            <Card className='border-0 rounded-0'>
+              <Card.Img className='rounded-0' src={product11} />
+
+              <Card.Body>
+                <Card.Title>Il Hwa Ginseng Extract</Card.Title>
+                <div>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                  <i class='fas fa-star mr-1 text-warning'></i>
+                </div>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
