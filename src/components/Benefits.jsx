203e22722b33d97d8cd0aadc034e22f561a95959
Phil <pjguitar15@gmail.com>
2021-03-12 13:24:59 +0800

    test

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Benefits = () => {
   return (
     <div>
       <Container>
         <Row>
-          <Col></Col>
+          <Col>Test</Col>
         </Row>
       </Container>
     </div>
