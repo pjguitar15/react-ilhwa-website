5e664e01355a07059a5e7e27ee54ac56a362a764
Phil <pjguitar15@gmail.com>
2021-03-12 16:45:09 +0800

    added front image cover

--- src/components/JumbotronComponent.jsx
+++ src/components/JumbotronComponent.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Jumbotron, Button, Container, Row, Col } from 'react-bootstrap'
-import jumbotronImg from '../Assets/jumbotron-img.jpg'
+
 const JumbotronComponent = () => {
   return (
-    <Jumbotron
-      className='m-0 rounded-0'
-      style={{ background: '#F8F9FE', padding: '18rem 0' }}
-    >
+    <Jumbotron className='m-0 rounded-0 jumbotron-section'>
       <Container>
         <Row>
           <Col md='12' lg='6' xs='12' className='text-center text-lg-left'>
-            <div className='front-page-h1 mb-4'>Il Hwa Philippines</div>
-            <p className='front-page-text mb-5'>
+            <div className='front-page-h1 mb-4 text-light'>
+              Il Hwa Philippines
+            </div>
+            <p className='front-page-text mb-5 text-light'>
               Korean Authenticated Ginseng Products, other herbal products,
               Vitamin, Supplement, Facial products etc.
             </p>
@@ -20,21 +19,6 @@ const JumbotronComponent = () => {
                 About Company
               </Button>
             </p>
-          </Col>
-          <Col className='img-col' md='12' lg='6' xs='12'>
-            <div
-              style={{
-                width: '100%',
-                height: '100%',
-                borderRadius: '50%',
-              }}
-            >
-              <img
-                style={{ borderRadius: '50%', height: '100%', width: '100%' }}
-                src={jumbotronImg}
-                alt='product'
-              />
-            </div>
           </Col>
         </Row>
       </Container>
